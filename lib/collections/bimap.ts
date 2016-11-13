b3c5da2ae181249dbcfcac7fa4b575520214d2af
Paul Sastrasinh <psastras@gmail.com>
2016-11-13 10:22:30 -0500

    clear out gh-pages

--- lib/collections/bimap.ts
+++
@@ -1,5 +0,0 @@
-interface BiMap<K, V> extends Map<K, V> {
-  inverse(): BiMap<V, K>;
-}
-
-export default BiMap;
--- lib/main.ts
+++
@@ -1,9 +0,0 @@
-import Numbers from "./primitives/numbers"
-import Objects from "./primitives/objects"
-import HashBiMap from "./collections/hashbimap"
-
-export default {
-  Objects,
-  HashBiMap,
-  Numbers
-};
